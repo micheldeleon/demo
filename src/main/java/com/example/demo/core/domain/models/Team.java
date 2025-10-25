8fe5c85dda40d2c0a79bb996eca272c123922bb5
Maximiliano Navarro <134528921+navarromaxi@users.noreply.github.com>
2025-10-25 13:31:33 -0300

    se agregan cosas al modelo

--- src/main/java/com/example/demo/core/domain/models/Team.java
+++ src/main/java/com/example/demo/core/domain/models/Team.java
@@ -11,7 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Team { // Equipo
     private Long id;
+    private String name; // nombre del equipo
     private List<Participant> participants; // participantes
     private User creator; // creador
+    //Fecha de creacion 
+    //Si vemos mas datos que pueden llegar a ir
 }
 
